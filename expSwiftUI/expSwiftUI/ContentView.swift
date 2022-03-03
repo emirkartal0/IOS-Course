e933f9b7df42d25aa113e805e633eddd72d3a40b
emir <emirkartal00@gmail.com>
2022-03-03 17:11:39 +0300

    swiftui

---
+++ expSwiftUI/expSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  expSwiftUI
+//
+//  Created by Emir Kartal on 3.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Eylül ❤ Emir")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ expSwiftUI/expSwiftUI/expSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  expSwiftUIApp.swift
+//  expSwiftUI
+//
+//  Created by Emir Kartal on 3.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct expSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
